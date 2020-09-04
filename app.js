d58e1f2161dfff7956838fe463d4ee820ae38951
Ankur Chakravarty <ankurchaks@gmail.com>
2020-09-05 04:51:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function GetData(Quescnt){
     const row=myTable[Quescnt];
     const columns=row.split(',');
     document.getElementById('Ques').textContent=columns[1];
-    
+
     document.getElementById('Option1').textContent=columns[2];
     document.getElementById('Option2').textContent=columns[3];
     document.getElementById('Option3').textContent=columns[4];
@@ -55,7 +55,7 @@ async function GetData(Quescnt){
     document.getElementById('Option2').className="btn btn-primary btn-sm btn-block";
     document.getElementById('Option3').className="btn btn-primary btn-sm btn-block";
     document.getElementById('Option4').className="btn btn-primary btn-sm btn-block";
-    
+
     document.getElementById('Announcement').style.display='none';
     document.getElementById('QuesNo').style.display='none';
     document.getElementById('Ans').style.display='none';
@@ -82,13 +82,13 @@ function callNextQuestion(){
         GetData(cnt);
         document.getElementById('QuesNo').textContent=cnt;
     }
-    
+
 }
 
 document.getElementById('BtnPrevious').addEventListener('click',callPreviousQuestion);
 
 function callPreviousQuestion(){
-    
+
     ResetScreen();
     const cnt=parseInt(document.getElementById('QuesNo').textContent)-1;
     GetData(cnt);
@@ -101,12 +101,13 @@ document.getElementById('Option3').addEventListener('click',callCheckOption3);
 document.getElementById('Option4').addEventListener('click',callCheckOption4);
 
 function callCheckOption1(){
-    
+
     var rightAns=document.getElementById('Ans').textContent;
     var myAns=document.getElementById('Option1').innerHTML;
     document.getElementById('Announcement').style.display='Block';
-    if (rightAns.trim()==myAns.trim()) { 
-        document.getElementById('Announcement').textContent='That\'s correct, '+myName ;
+    if (rightAns.trim()==myAns.trim()) {
+        var myName=document.getElementById("inputDefault").value
+        document.getElementById('Announcement').textContent='That\'s correct, '+ myName ;
         document.getElementById('Announcement').style.color='green';
         document.getElementById('Option1').className="form-control is-valid btn-sm"
        // document.getElementById('Option1').className="btn btn-success btn-sm btn-block"
@@ -123,7 +124,7 @@ function callCheckOption2(){
     var myAns=document.getElementById('Option2').innerHTML;
     var myName=document.getElementById("inputDefault").value
     document.getElementById('Announcement').style.display='Block';
-    if (rightAns.trim()==myAns.trim()) { 
+    if (rightAns.trim()==myAns.trim()) {
         document.getElementById('Announcement').textContent='Great '+myName+'! you are a genius';
         document.getElementById('Announcement').style.color='green';
         document.getElementById('Option2').className="form-control is-valid btn-sm"
@@ -140,7 +141,7 @@ function callCheckOption3(){
     var myAns=document.getElementById('Option3').innerHTML;
     var myName=document.getElementById("inputDefault").value
     document.getElementById('Announcement').style.display='Block';
-    if (rightAns.trim()==myAns.trim()) { 
+    if (rightAns.trim()==myAns.trim()) {
         document.getElementById('Announcement').textContent='Super '+myName+'! you have got it right';
         document.getElementById('Announcement').style.color='green';
         document.getElementById('Option3').className="form-control is-valid btn-sm"
@@ -157,7 +158,7 @@ function callCheckOption4(){
     var myAns=document.getElementById('Option4').innerHTML;
     var myName=document.getElementById("inputDefault").value
     document.getElementById('Announcement').style.display='Block';
-    if (rightAns.trim()==myAns.trim()) { 
+    if (rightAns.trim()==myAns.trim()) {
         document.getElementById('Announcement').textContent='Wow '+myName+'! you have got it right';
         document.getElementById('Announcement').style.color='green';
         document.getElementById('Option4').className="form-control is-valid btn-sm"
@@ -167,8 +168,3 @@ function callCheckOption4(){
         document.getElementById('Option4').className="form-control is-invalid btn-sm"
     }
 }
-
-
-
-
-    
